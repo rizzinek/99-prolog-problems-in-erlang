1931a515816f7f1dd31c7da5f0b707e6c223c3c8
rizzinek <the.rizzin@gmail.com>
2016-02-10 02:26:21 +0000

    p02 now uses funcTester too; have to continue to modify other solutions and add time measurement for the tests

---
+++ p02/p02.erl
@@ -0,0 +1,26 @@
+-module(p02).
+-export([p02/0]).
+
+-include("../funcTester/funcTester.hrl").
+
+%find the second to last element of a list
+
+p1([X | [_]]) ->
+    X;
+p1([_ | Tail]) ->
+    p1(Tail).
+
+p02() ->
+	code:add_path("../funcTester"),
+	code:load_file(funcTester),
+	Funcs = [
+	#testedFunction{description="my tail-recursive version of finding the second to last element", funcObject=fun p1/1}
+	, #testedFunction{description="lists:nth(lists:reverse)", funcObject=fun(X) -> lists:nth(2, lists:reverse(X)) end}
+	, #testedFunction{description="hd(lists:nthtail)", funcObject=fun(X) -> hd(lists:nthtail(length(X) - 2, X)) end}],
+	Tests = [
+	#singleTest{arguments = [[a, b, c, d]], expectedValue = c}
+	, #singleTest{arguments = [[]], expectedValue = error}
+	, #singleTest{arguments = [[123]], expectedValue = error}
+	, #singleTest{arguments = [[123, 456]], expectedValue = 123}
+	],
+	funcTester:performTesting(Funcs, Tests).
